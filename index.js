0723b9d163905cfdca62fc52d2ed5b00b3d705a2
dani31perez <perezbarredad@gmail.com>
2025-03-15 16:09:56 -0600

    Added GET route to fetch a book by ID

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ let books = [
     id: 1,
     title: "Cien años de soledad",
     author: "Gabriel García Márquez",
-    language: "Español",
-  }
+    language: "Spanish",
+  },
 ];
 
-app.get("api/books", (_req, res) => {
-    res.json(books);
-});  
+app.get("/api/books", (_req, res) => {
+  res.json(books);
+});
+
+app.get("/api/books/:id", (req, res) => {
+  let idBook = parseInt(req.params.id);
+  let book = books.find((book) => (book.id = idBook));
+  if (!book) return res.status(404).json({ error: "Book not found." });
+  res.json(book);
+});
 
 app.use((err, _req, res, _next) => {
   console.error(err.stack);
